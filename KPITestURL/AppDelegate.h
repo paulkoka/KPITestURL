da01a73dad5c27cff139ee54c6e825f0a7d6a3e7
paulkoka <pavelkoka@gmail.com>
2018-05-29 12:42:49 +0300

    Initial Commit

---
+++ KPITestURL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KPITestURL
+//
+//  Created by paul on 5/29/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KPITestURL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KPITestURL
+//
+//  Created by paul on 5/29/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
